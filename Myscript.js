abe08b8b8ea9e8b69590c8bed2a7336e40aae7c8
jefhwite <5fd274a2@opayq.com>
2015-07-23 18:39:37 -0700

    Swaps "there" with "their" to Grammar Nazi's despair
    This is my attempt at coding. I didn’t do much at all, thanks to Steven
    Frank of Cloud to Butt. As my understanding of programming develops,
    I’d like to enrich There to Their with more elements. There. Their.
    They’re? Time will tell.

---
+++ Myscript.js
@@ -0,0 +1,40 @@
+//credit goes to Steven Frank of Cloud to Butt (https://github.com/panicsteve/cloud-to-butt/) and Lauren Orsini of caaaaarbs.
+
+walk(document.body);
+
+function walk(node)  
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1: 
+		case 9:  
+		case 11: 
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling; 
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: 
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bthere\b/g, "their");
+	v = v.replace(/\bThere\b/g, "Their");
+	
+	textNode.nodeValue = v;
+} 
